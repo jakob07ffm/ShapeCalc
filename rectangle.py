092f6ec79af0fa056a1a711243c9d5b5ad57066b
Jakob__init__ <jsuetfels@gmail.com>
2024-08-24 10:09:41 +0200

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -3,43 +3,49 @@ import sys
 
 pygame.init()
 
+
 width, height = 1000, 800
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("RECTANGLE")
 
 clock = pygame.time.Clock()
 
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+
 
 font = pygame.font.Font(None, 32)
 text_heading = font.render('RECTANGLE', True, BLACK)
 text_heading_rect = text_heading.get_rect()
 text_heading_rect.center = (width // 2, 40)
 
-big_rect_img = pygame.image.load("Shape_img/RECTANGLE.png")
-big_rect_img_size = big_rect_img.get_size()
-scale_factor = 0.5
-rect_img_size = (int(big_rect_img_size[0] * scale_factor), int(big_rect_img_size[1] * scale_factor))
-rect_img = pygame.transform.scale(big_rect_img, rect_img_size)
 
-# Read the file line by line
-with open("formular/formular.txt", "r") as formular_file:
-    lines = formular_file.readlines()
+rect_img_size = (500, 200)  
+rect_img = pygame.Surface(rect_img_size)
+rect_img.fill(BLACK)
 
-# Render each line of the text separately
+
+lines = [
+    "This is a rectangle.",
+    "The area is calculated as:",
+    "Area = width * height",
+    "Enter width and height below:"
+]
+
+
 rendered_text_lines = []
-y_offset = 80  # Starting Y position for the text
-line_height = font.get_height()  # Height of each line of text
+y_offset = 80  
+line_height = font.get_height()  
 
 for line in lines:
-    rendered_text = font.render(line.strip(), True, BLACK)
+    rendered_text = font.render(line, True, BLACK)
     rendered_text_rect = rendered_text.get_rect()
     rendered_text_rect.topleft = (10, y_offset)
     rendered_text_lines.append((rendered_text, rendered_text_rect))
     y_offset += line_height
 
-# Input box settings
+
 input_boxes = [
     {'rect': pygame.Rect(500, 60, 140, 32), 'color': pygame.Color("black"), 'text': '', 'active': False},
     {'rect': pygame.Rect(500, 100, 140, 32), 'color': pygame.Color("black"), 'text': '', 'active': False}
@@ -47,6 +53,7 @@ input_boxes = [
 
 input_color_active = pygame.Color("azure3")
 input_color_passive = pygame.Color("black")
+
 
 running = True
 while running:
@@ -82,7 +89,7 @@ while running:
         win.blit(text_surface, (box['rect'].x + 5, box['rect'].y + 5))
         box['rect'].w = max(100, text_surface.get_width() + 10)
 
-    # Calculate and display the result
+
     try:
         value1 = int(input_boxes[0]['text'])
         value2 = int(input_boxes[1]['text'])
@@ -93,7 +100,6 @@ while running:
         pass
 
     pygame.display.flip()
-
     clock.tick(30)
 
 pygame.quit()
